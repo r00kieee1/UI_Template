bfa57fb9b6e068f8743c9d4046c32baf6ad5e52d
r00kieee <omustapha@005@gmail.com>
2023-07-07 18:59:18 +0100

    first push

---
+++ Template.js
@@ -0,0 +1,9 @@
+const button = document.getElementsByClassName('button');
+const collapsible = document.getElementsByClassName('collapsible');
+button.forEach
+
+
+function toggleCollapsible() {
+    var content = document.querySelector('.collapsible-content');
+    content.classList.toggle('show');
+  }
